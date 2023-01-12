9d202df881ba0f2ec64f91266e155386b1bb95d5
justin.park <justin.ad.park@gmail.com>
2023-01-12 17:23:38 +0900

    함수형 인터페이스를 활용한 람다 리팩토링

--- src/main/java/com/example/javaLang/generic/wildcards/Person.java
+++ src/main/java/com/example/javaLang/generic/wildcards/Person.java
@@ -2,15 +2,22 @@ package com.example.javaLang.generic.wildcards;
 
 public class Person {
     private String name;
+    private int age;
 
     public Person(String name) {
         this.name = name;
     }
+    public Person(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
 
     public String getName() { return name; }
+    public int getAge() { return age; }
 
     @Override
     public String toString() {
-        return name;
+        return name + " : " + age;
     }
 }
--- src/test/java/com/example/javaLang/generic/wildcards/WildcardsTest.java
+++ src/test/java/com/example/javaLang/generic/wildcards/WildcardsTest.java
@@ -1,4 +1,83 @@
 package com.example.javaLang.generic.wildcards;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class WildcardsTest {
+
+    private Person youngestPerson = new Person("홍길동",17);
+    private Person intermediatePerson = new Person("이몽룡",21);
+    private Person ahphabetFirstPerson = new Person("김유신",28);
+
+    private Supplier<Stream<Person>> peopleSupplier = () -> Stream.of(youngestPerson, intermediatePerson, ahphabetFirstPerson);
+
+    private Stream<Person> getPersonStream() {
+        return peopleSupplier.get();
+    }
+
+    private final Comparator<Person> compareName = (p1, p2) -> p1.getName().compareToIgnoreCase(p2.getName());
+    private final Comparator<Person> compareAge = (p1, p2) -> Integer.compare(p1.getAge(), p2.getAge());
+
+    /**
+     * 전체 인원 중 가나다 순으로 이름이 가장 빠른 인원
+     * 최연소 구성원
+     */
+    @Test
+    void windcardTest() {
+        // 방법 1 (스트림 로직으로 풀어본다.)
+        String alphaFirstName = getPersonStream()
+                .map(Person::getName)
+                .min(String::compareToIgnoreCase)
+                .get();
+        System.out.println(alphaFirstName);
+
+        Assertions.assertEquals(ahphabetFirstPerson.getName(), alphaFirstName);
+
+        int minAge = getPersonStream()
+                .mapToInt(Person::getAge)
+                .min()
+                .getAsInt();
+        System.out.println(minAge);
+        Assertions.assertEquals(youngestPerson.getAge(), minAge);
+
+        // 방법 2 : min(비교 로직)
+        Person alphaFirst = getPersonStream()
+                .min((t1, t2) ->  t1.getName().compareToIgnoreCase(t2.getName()))
+                .get();
+        System.out.println("가나다순 : " + alphaFirst);
+        Assertions.assertEquals(ahphabetFirstPerson, alphaFirst);
+
+        Person youngest = getPersonStream()
+                .min((t1, t2) ->  Integer.compare(t1.getAge(), t2.getAge()))
+                .get();
+        System.out.println("최연소 : " + youngest);
+        Assertions.assertEquals(youngestPerson.getAge(), minAge);
+
+        // 방법 3 : Comparator<T> interface를 이용
+        alphaFirst = getPersonStream().min(compareName).get();
+        System.out.println("가나다 : " + alphaFirst);
+        Assertions.assertEquals(ahphabetFirstPerson, alphaFirst);
+
+        youngest = getPersonStream().min(compareAge).get();
+        System.out.println("최연소 : " + youngest);
+        Assertions.assertEquals(youngestPerson.getAge(), minAge);
+
+
+        // 방법 4 : Comparator<T>로 반복 메소드 분리
+        alphaFirst = getMinPerson(getPersonStream(), compareName);
+        System.out.println("가나다 : " + alphaFirst);
+
+        youngest = getMinPerson(getPersonStream(), compareAge);
+        System.out.println("최연소 : " + youngest);
+
+    }
+
+    private <T extends Person> T getMinPerson(Stream<T> p, Comparator<T> comparator) {
+        return p.min(comparator).get();
+
+    }
 }
