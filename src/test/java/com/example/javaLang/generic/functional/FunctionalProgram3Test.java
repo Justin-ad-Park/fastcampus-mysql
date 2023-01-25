024d5ecd6d5a77b398ba3eaddf577115ec6ec822
justin.park <justin.ad.park@gmail.com>
2023-01-25 16:51:13 +0900

    함수형 프로그래밍 예제 C4

--- src/test/java/com/example/javaLang/generic/functional/FunctionalProgram3Test.java
+++ src/test/java/com/example/javaLang/generic/functional/FunctionalProgram3Test.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.Test;
 import java.util.function.*;
 import java.util.stream.Stream;
 
+// 함수형 인터페이스 Predicate<? super T> 를 인자로 요구하는 스트림 filter 메서드 사용 예)
 public class FunctionalProgram3Test {
 
     private <T> void printStream(Stream<T> t) {
         t.forEach(System.out::println);
     }
 
+
+    // 1. 직접 익명 클래스를 만들어 인터페이스를 구현한 메서드를 제공한다.
     @Test
     void inheritFilterTest() {
 
@@ -28,6 +31,7 @@ public class FunctionalProgram3Test {
         printStream(youngStudents);
     }
 
+    // 2. 익명 클래스를 생성하는 팩토리 메서드를 통해 제공한다. 1번의 확장 개념
     private static Predicate<Student> YoungFilter() {
         return new Predicate<Student>() {
             @Override
@@ -46,6 +50,7 @@ public class FunctionalProgram3Test {
     }
 
 
+    // 3. 함수형 인터페이스를 구현한 클래스를 만들어 사용한다.
     class YoungFilterClass<T extends Student> implements Predicate<T> {
 
         @Override
@@ -67,6 +72,7 @@ public class FunctionalProgram3Test {
 
     }
 
+    // 4. Lambda 표현식으로 제공하거나, Lambda 표현식을 변수에 할당해서 제공한다.
     @Test
     void lambdaTest() {
         Stream<Student> students = StreamTestGenerator.getPersonStream();
@@ -80,7 +86,7 @@ public class FunctionalProgram3Test {
 
     }
 
-
+    // 4-1. print도 Lambda 표현식을 변수에 할당하면 아래와 같이 간소화
     @Test
     void allLambdaTest() {
         Stream<Student> students = StreamTestGenerator.getPersonStream();
---
+++ src/test/java/com/example/javaLang/generic/functional/FunctionalProgram4Test.java
@@ -0,0 +1,100 @@
+package com.example.javaLang.generic.functional;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class FunctionalProgram4Test {
+
+    private static <T> Consumer<T> printAny() {
+        return t -> System.out.println(t.toString());
+    }
+
+    private static Consumer<? super Number> printNumber() {
+        return t -> System.out.println(t.toString());
+    }
+    // 함수형 인자로 Comsumer<T> 가능
+    private static <T> void printAnyByFunction(Consumer<T> prt, T t) {
+        prt.accept(t);
+    }
+
+    //제너릭 <T extends String>으로 선언 후 함수형 인자로 <? super T> 가능
+    //왜냐? T exteds String 조건에 만족하는 인자는 모두 <? Super T> 조건에 부합하니까 가능함
+    private static <T extends String> void printStringByFunction(Consumer<? super T> prt, T t) {
+        prt.accept(t);
+    }
+
+    // <T extends String>를 <? extends T>로 변환은 불가능
+    // String에게 3세대의 자식 클래스 계층이 있다고 할 때,
+    // 3세대를 2세대로 변환은 가능하지만,
+    // 2세대를 3세대로 변환은 불가능
+    // (부모는 자식을 품을 수 있지만, 자식은 부모를 품지 못한다는 인생의 진리가 개발 언어에도 녹가 있는...)
+    /*
+    private static <T extends String> void printStringByFunction(Consumer<? extends T> prt, T t) {
+        prt.accept(t);
+    }
+    */
+
+
+    @Test
+    void test() {
+        Consumer<String> prtAny = printAny();
+        prtAny.accept("Hello world!!");
+
+        var prtNumber = printNumber();
+        prtNumber.accept(1);
+
+        Stream<String> s = Arrays.asList("Test", "Abc", "Hello List").stream();
+        s.forEach(prtAny);
+
+        Stream<Long> sl = Arrays.asList(1L, 2L, 100L).stream();
+        sl.forEach(prtNumber);
+
+        printStringByFunction(printAny(), "이건 된다.");
+        //printStringByFunction(printNumber(), "이건 안된다.");
+        printAnyByFunction(printAny(), 1L);
+        printAnyByFunction(printNumber(), 1L);
+
+    }
+
+    @Test
+    void test2() {
+        // 하나 더 해보자.
+        Consumer<? super String> prtLambda = (t) -> System.out.println(t);
+        prtLambda.accept( "Test" );
+        printAnyByFunction(prtLambda, "Test");
+    }
+
+    Runnable r = () -> System.out.println("Running");
+
+    @Test
+    void run() {
+        r.run();
+    }
+
+    @FunctionalInterface
+    interface Action<T> {
+        void act(T t);
+    }
+
+    public void execute(Runnable runnable) {
+        runnable.run();
+    }
+    public <T extends String> void execute(Action<? super String> action, T t) {
+        action.act(t);
+    }
+
+    @Test
+    void act() {
+        var t = "Actor";
+        execute((u) -> System.out.println(u + "'s Action"), t);
+        execute(() -> System.out.println("Runnable"));
+
+        execute((Action)(u) -> System.out.println(u + "'s Action2"), t);
+        execute((Runnable)() -> System.out.println("Runnable2"));
+    }
+
+
+}
