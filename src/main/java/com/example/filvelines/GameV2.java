a15b3a8e0169bcafb5b5940c4f0fa778b9ee72d1
justin.park <justin.ad.park@gmail.com>
2025-01-14 15:41:57 +0900

    Method Extract by 5Lines code

--- src/main/java/com/example/filvelines/GameV2.java
+++ src/main/java/com/example/filvelines/GameV2.java
@@ -112,34 +112,46 @@ public class GameV2 extends JPanel implements KeyListener {
     private void handleInputs() {
         while (!inputs.isEmpty()) {
             Input input = inputs.remove(0);
-            keyPressCount++;
-            switch (input) {
-                case LEFT -> moveHorizontal(-1);
-                case RIGHT -> moveHorizontal(1);
-                case UP -> moveVertical(-1);
-                case DOWN -> moveVertical(1);
-            }
 
+            countKeyPress();
+            handleInput(input);
             repaint();
             checkSuccess();
+        }
+    }
+
+    private void countKeyPress() {
+        keyPressCount++;
+    }
+
+    private void handleInput(Input input) {
+        switch (input) {
+            case LEFT -> moveHorizontal(-1);
+            case RIGHT -> moveHorizontal(1);
+            case UP -> moveVertical(-1);
+            case DOWN -> moveVertical(1);
         }
     }
 
     private void updateMaps() {
         for (int y = map.length - 2; y >= 0; y--) {
             for (int x = 0; x < map[y].length; x++) {
-                if ((map[y][x] == Tile.STONE || map[y][x] == Tile.FALLING_STONE) && map[y + 1][x] == Tile.AIR) {
-                    map[y + 1][x] = Tile.FALLING_STONE;
-                    map[y][x] = Tile.AIR;
-                } else if ((map[y][x] == Tile.BOX || map[y][x] == Tile.FALLING_BOX) && map[y + 1][x] == Tile.AIR) {
-                    map[y + 1][x] = Tile.FALLING_BOX;
-                    map[y][x] = Tile.AIR;
-                } else if (map[y][x] == Tile.FALLING_STONE) {
-                    map[y][x] = Tile.STONE;
-                } else if (map[y][x] == Tile.FALLING_BOX) {
-                    map[y][x] = Tile.BOX;
-                }
+                updateTile(y, x);
             }
+        }
+    }
+
+    private void updateTile(int y, int x) {
+        if ((map[y][x] == Tile.STONE || map[y][x] == Tile.FALLING_STONE) && map[y + 1][x] == Tile.AIR) {
+            map[y + 1][x] = Tile.FALLING_STONE;
+            map[y][x] = Tile.AIR;
+        } else if ((map[y][x] == Tile.BOX || map[y][x] == Tile.FALLING_BOX) && map[y + 1][x] == Tile.AIR) {
+            map[y + 1][x] = Tile.FALLING_BOX;
+            map[y][x] = Tile.AIR;
+        } else if (map[y][x] == Tile.FALLING_STONE) {
+            map[y][x] = Tile.STONE;
+        } else if (map[y][x] == Tile.FALLING_BOX) {
+            map[y][x] = Tile.BOX;
         }
     }
 
@@ -148,7 +160,19 @@ public class GameV2 extends JPanel implements KeyListener {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        //Draw Map
+        paintMap(g);
+
+        paintPlayer(g);
+    }
+
+    private void paintPlayer(Graphics g) {
+        //Paint Player
+        g.setColor(Color.RED);
+        g.fillRect(playerX * TILE_SIZE, playerY * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    }
+
+    private void paintMap(Graphics g) {
+        //Paint Map
         for (int y = 0; y < map.length; y++) {
             for (int x = 0; x < map[y].length; x++) {
                 if (map[y][x] == Tile.FLUX) {
@@ -170,10 +194,6 @@ public class GameV2 extends JPanel implements KeyListener {
                 g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
             }
         }
-
-        //Daw Player
-        g.setColor(Color.RED);
-        g.fillRect(playerX * TILE_SIZE, playerY * TILE_SIZE, TILE_SIZE, TILE_SIZE);
     }
 
     @Override
